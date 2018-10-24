c37df7830106670458a35941d63bc8db0a788d2e
Ramit B <ramitb@rocketmail.com>
2018-10-23 21:25:27 -0400

    R Script that runs sigfeature

--- sigFeaturepredicting.R
+++ sigFeaturepredicting.R
@@ -9,7 +9,7 @@ sigFeature.model=svm(x[ ,sigfeatureRankedList], y,type="C-classification", kerne
 pred <- predict(sigFeature.model, x[ ,sigfeatureRankedList]) #predicts crossvalidation results(default is 10-fold)
 y <- y[,1] #mateches dimensions to that of pred
 table(pred,y)
-system.time(featureRankedList <- svmrfeFeatureRanking(x, y)) #optional command that runs another svm-rfe model that's slower than sigfeatiure
+system.time(featureRankedList <- svmrfeFeatureRanking(x, y)) #optional command that runs another svm-rfe model that is slower than sigfeatiure
 pred <- predict(RFE.model, x[ ,featureRankedList])
 table(pred,y) #outputs table with True positives, False positives, True negatives and False negatives.
 table <- table(pred,y)
